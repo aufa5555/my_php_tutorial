54a09b31dcbb9b0644e7e3811d3fc5907e061678
aufa5555 <meri31705@gmail.com>
2026-08-26 18:06:33 +0800

    numbers

---
+++ tuto/tuto_6.php
@@ -0,0 +1,47 @@
+<?php
+
+$radius = 25; // whole number dikira INT
+$pi = 3.14;  //kalau ada titik float
+
+// basic  - *, /, +, -, **
+
+//echo $pi * $radius**2;
+
+// order of operation ( B I D M A S )
+
+//echo 2 * (4+9) / 3;
+
+// increment & decrement operators
+
+//echo $radius--;
+//echo $radius;
+
+// shorthand operators
+
+$age = 20;
+
+//$age -=10;
+//$age *= 2;
+//cho $age;
+
+// number unctions
+
+//echo floor($pi);
+//echo ceil($pi);
+echo pi();
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> PHP Tutorials</title>
+</head>
+<body>
+
+</body>
+</html>
